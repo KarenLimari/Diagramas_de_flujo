4f273a531f37e4876d295fc206d6a245d6280c3a
Karen Limari <karenlimari20@gmail.com>
2024-07-25 01:27:26 +0200

    codigo creado y ordenado paso a paso

--- ejercicioguiado1.py
+++ ejercicioguiado1.py
@@ -2,6 +2,13 @@
 #Paso 1
 #Realizar una tabla para desglosar el problema en tres partes:
 #● datos de entrada
+import math
+#variables
+numero1 = float(input("ingrese un numero del 1 al 100: \n>"))
+numero2 = float(input("ingrese un numero del 1 al 100: \n>"))
 #● proceso
+suma = numero1 + numero2
+suma = math.ceil(suma)
 #● datos de salida
+print(f"La suma de ambos numero es: {suma}")
 
